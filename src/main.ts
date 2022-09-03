20a23aee3f669034043640cf34b0602269bab81e
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-09-03 11:59:07 +0300

    first config
    first config

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  await app.enableCors();
+  await app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Smart Booking Backend')
+    .setDescription('Api for smart booking application')
+    .setVersion('1.0.0')
+    .addTag('SmartBooking', 'Application for appointment services online')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('sw', app, document);
+  await app.listen(3000);
+}
+bootstrap().then();
