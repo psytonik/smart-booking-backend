23e6cdcd4ea8cf5cbaee0b98217db86e61c4c771
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-09-04 21:24:56 +0300

    adding user service to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @HttpCode(200)
   @Get('/')
-  async getUsers() {
-    return 'Hello world';
+  async getUsers(): Promise<any> {
+    return await this.userService.getUser();
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async getUser() {
+    return { status: 'hello from user service' };
+  }
+}
