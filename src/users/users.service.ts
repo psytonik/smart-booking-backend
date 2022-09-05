82458bcd54fff3125621d55211badaa9243ae11c
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-09-05 12:25:18 +0300

    adding check for existing user, fix issue with duplicate key

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { UserDocument, User } from './users.model';
 import { Model } from 'mongoose';
@@ -12,11 +12,23 @@ export class UsersService {
   ) {}
 
   async createUser(userDto: UserDto): Promise<User> {
+    await this.checkExistingEmail(userDto.email);
     const newUser = new this.usersModel(userDto);
     return await newUser.save();
   }
 
   async getUser(): Promise<any> {
     return this.usersModel.find();
+  }
+
+  async checkExistingEmail(email: string) {
+    const user = this.usersModel.findOne({ email });
+    if (user) {
+      throw new HttpException(
+        'User with this email already exists',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    return;
   }
 }
