d418fed2a2c50ad8966a695faa148212006a9c89
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-09-06 00:16:21 +0300

    adding business controller service module small fix

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BusinessController } from './business.controller';
 import { BusinessService } from './business.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature()],
   controllers: [BusinessController],
   providers: [BusinessService],
 })
