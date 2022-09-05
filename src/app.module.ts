b8986f056467d47eb041fa16db8c7a1103cbd3c4
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-09-06 00:15:03 +0300

    adding business controller service module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,19 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { getMongoConfig } from './config/mongo.config';
+import { BusinessModule } from './business/business.module';
 
 @Module({
   imports: [
-    AuthModule,
-    UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: getMongoConfig,
     }),
+    BusinessModule,
+    AuthModule,
+    UsersModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
