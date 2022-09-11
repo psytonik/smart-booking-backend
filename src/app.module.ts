acb7918f0c3ac79121aacbcfaf0cdec90965257a
Anthony Fink <40363736+psytonik@users.noreply.github.com>
2022-09-12 00:18:53 +0300

    adding slugify to project modify user model, adding user by slug,

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -15,7 +14,6 @@ import { BusinessModule } from './business/business.module';
       useFactory: getMongoConfig,
     }),
     BusinessModule,
-    AuthModule,
     UsersModule,
   ],
 })
